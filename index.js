8db7f4a2cd2fb4b4a5362693b07d81845189acf1
hungtt <hungtddgcd201514@fpt.edu.vn>
2022-03-14 09:08:43 +0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-import fs from 'fs'
 import fetch from 'node-fetch'
 import puppeteer from 'puppeteer'
 
-let data_conv = fs.readFileSync('data_conv.json', 'utf8')
-data_conv = JSON.parse(data_conv);
 let checkURL = {}
 
 import express from 'express'
@@ -31,95 +28,6 @@ app.get('/', function (req, res) {
 
 app.get('/ip', function (req, res) {
     res.send(JSON.stringify({"ip":req.headers['x-forwarded-for']}));
-})
-
-app.post('/send_message', function (req, res) {
-    let data_post = req.body;
-    let message = data_post.text;
-    let user_id = data_post.token;
-    let api_test = data_post.api_test;
-    if(data_conv[user_id]){
-        data_conv[user_id].push({
-            'text': message,
-            'rule': 'user'
-        });
-    }
-    else{
-        data_conv[user_id] = [{
-            'text': message,
-            'rule': 'user'
-        }];
-    }
-    fetch(api_test,{
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            "text": message,
-            "meta":{
-                "user_id": ""
-            }
-        })
-    })
-    .then(response => response.json())
-    .then(data => {
-        let last_mess = data_conv[user_id][data_conv[user_id].length-1];
-        let last_time_stamp = new Date().getTime();
-        if(last_mess.rule == 'bot'){
-            last_time_stamp = last_mess.time_stamp;
-        }
-        data_conv[user_id].push({
-            'text': data.text,
-            'rule': 'bot',
-            'time_stamp': new Date().getTime()
-        });
-        fs.writeFileSync('data_conv.json', JSON.stringify(data_conv));
-        data['rule'] = 'bot';
-        let last_online = new Date().getTime()-last_time_stamp;
-        if(last_online > 60000){
-            // convert to minutes or hours
-            if(last_online > 3600000){
-                data['time_stamp'] = Math.floor(last_online/3600000)+' hours ago';
-            }else{
-                data['time_stamp'] = Math.floor(last_online/60000)+' minutes ago';
-            } 
-        }else{
-            data['time_stamp'] = 'Just now';
-        }
-        res.send(data);
-    })
-    .catch(error => console.error(error))
-})
-
-
-app.post('/get_message', function (req, res) {
-    let data_post = req.body;
-    let user_id = data_post.token;
-    // if(req.headers['x-forwarded-for']){
-    //     user_id = req.headers['x-forwarded-for']+'_'+user_id;
-    // }
-    if(data_conv[user_id]){
-        let last_mess = data_conv[user_id][data_conv[user_id].length-1];
-        let last_time_stamp = new Date().getTime();
-        if(last_mess.rule == 'bot'){
-            last_time_stamp = last_mess.time_stamp;
-        }
-        let last_online = new Date().getTime()-last_time_stamp;
-        if(last_online > 60000){
-            // convert to minutes or hours
-            if(last_online > 3600000){
-                last_online = Math.floor(last_online/3600000)+' hours ago';
-            }else{
-                last_online = Math.floor(last_online/60000)+' minutes ago';
-            }
-        }else{
-            last_online = 'Just now';
-        }
-        res.send(JSON.stringify({"results":data_conv[user_id], "time_stamp":last_online}));
-    }else{
-        res.send(JSON.stringify({"results":[], "time_stamp":"offline"}));
-    }
 })
 
 async function crawl(url) {
@@ -164,7 +72,9 @@ async function crawl(url) {
 
 app.get('/data_web', function(req, res){
     let url = req.query.url;
-    if(url in checkURL){
+    let main_url = url.split('/');
+    main_url = main_url[0]+'//'+main_url[2];
+    if(url in checkURL || main_url in checkURL || checkURL[url] || checkURL[main_url]){
         return res.send(checkURL[url]);
     }else{
         const result = crawl(url);
@@ -172,13 +82,10 @@ app.get('/data_web', function(req, res){
             checkURL[url] = data;
             res.send(data);
         }).catch(function(err){
-            let url_get = req.query.url;
-            url_get = url_get.split('/');
-            let main_url = url_get[0]+'//'+url_get[2];
-            console.log(main_url);
             const result = crawl(main_url);
             result.then(function(data){
                 checkURL[url] = data;
+                checkURL[main_url] = data;
                 res.send(data);
             })
         });
